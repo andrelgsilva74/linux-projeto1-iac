858061ec3797f01961636184b0011bd8ec5885ee
Andre Luis <andrelgsilva74>
2022-08-27 18:27:12 -0300

    Backup dos scripts

---
+++ linux-projeto1-iac/Editado/criar_user.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+echo "Criando usuários convidados no sistema..."
+
+useradd guest10 -c "User Convidado" -s /bin/bash -m
+passwd guest10
+passwd guest10 -e
+
+useradd guest11 -c "User Convidado" -s /bin/bash -m
+passwd guest11
+passwd guest11 -e
+
+useradd guest12 -c "User Convidado" -s /bin/bash -m
+passwd guest12
+passwd guest12 -e
+
+useradd guest13 -c "User Convidado" -s /bin/bash -m
+passwd guest13
+passwd guest13 -e
+
+echo "Finalizado!!"
